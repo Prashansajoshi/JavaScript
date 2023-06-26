105df49de088c31d7ec45afbd4eb0d1cbdd42055
prashansa Joshi <joshiprashansa11@gmail.com>
2023-06-26 02:24:34 -0700

    Condition in JS

---
+++ index.js
@@ -0,0 +1,21 @@
+//arrays
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors.length);
+
+
+//functions
+// function greet(name, lastName) {     //name=parameter
+//     console.log('Hello ' + name + ' ' + lastName);
+// }
+
+// greet('Anna', 'Smith');    //anna = argument i.e supplied to the parameter
+
+
+//Calculating a value
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
