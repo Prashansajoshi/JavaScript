5234a71ef24e8291ef8a18d09251cad575bbe13e
Prashansajoshi <121188986+Prashansajoshi@users.noreply.github.com>
2023-06-28 04:06:34 -0700

    Add files via upload

---
+++ Loop/forloop.js
@@ -0,0 +1,6 @@
+    for(var i=0; i<5; i++)
+    console.log("current value of i:" +i);
+
+    console.log("\nPrinting Even Numbers")
+    for(num=0; num<10; num=num+2)  //for even numbers
+    console.log(num);
